94cd90a40459329cffef79e7eebfc064d6d517f8
PeterRapcsak <peter.rapcsak@gmail.com>
2024-01-13 11:27:19 +0100

    fak

--- src/youtubedl.js
+++ src/youtubedl.js
@@ -1,41 +1,35 @@
-window.onload = function(){
+window.onload = function () {
 
   var videoDetails = ytplayer.config.args.player_response;
   var videoDetailsParse = JSON.parse(videoDetails);
   var videoDetailsCut = videoDetailsParse["streamingData"]["adaptiveFormats"];
-  var body = document.getElementsByTagName('body')[0];
 
-  function clicked(e) {
-    if(e.target.id == "text")
-      clickDownload();
-    else
-      removeDropdown();
+  function removeDropdown() {
+      var li = document.querySelectorAll("#download-dropdown ul li");
+      for (var i = 0; i < li.length; i++) {
+          if (li[i].style.display === "block") {
+              li[i].style.display = "";
+          }
+      }
   }
 
-  function removeDropdown(e) {
-    console.log(e);
-    for(i=0;i<li.length;i++) {
-      if(li[i].style.display == "block")
-        li[i].style.display = "";
-    }
+  function clickDownload() {
+      var li = document.querySelectorAll("#download-dropdown ul li");
+      for (var i = 0; i < li.length; i++) {
+          if (li[i].style.display === "") {
+              li[i].style.display = "block";
+          } else {
+              li[i].style.display = "";
+          }
+      }
   }
 
-  function clickDownload(){
-    console.log("hello");
-    for(i=0;i<li.length;i++) {
-      if(li[i].style.display == "")
-        li[i].style.display = "block";
-      else
-        li[i].style.display = "";
-    }
-  }
-
-  function getNameUrl(e){
-    var url = e.currentTarget.getAttribute("href");
-    var name = document.getElementsByTagName("title")[0].innerText;
-    var datatype = e.currentTarget.getAttribute("data-type");
-    var data = {url: url, name: name, sender: "YTDL", type: datatype};
-    window.postMessage(data, "*");
+  function getNameUrl(e) {
+      var url = e.currentTarget.getAttribute("href");
+      var name = document.querySelector("title").innerText;
+      var datatype = e.currentTarget.getAttribute("data-type");
+      var data = { url: url, name: name, sender: "YTDL", type: datatype };
+      window.postMessage(data, "*");
   }
 
   var btn = document.createElement("ytd-button-renderer");
@@ -45,18 +39,19 @@ window.onload = function(){
   btn.innerText = "Download";
 
   var anchor = document.createElement("a");
-  anchor.id = "anchor"
-  anchor.setAttribute("tab-index", "-1");
+  anchor.id = "anchor";
+  anchor.setAttribute("tabindex", "-1");
   anchor.className = "yt-simple-endpoint style-scope ytd-button-renderer";
 
   var btn_icon = document.createElement("yt-icon-button");
   btn_icon.className = "style-scope ytd-button-renderer style-default size-default";
-  btn_icon.id = "button"
+  btn_icon.id = "button";
 
   var string = document.createElement("p");
   string.id = "text";
-  string.className = "style-scope ytd-button-renderer style-default size-default"
+  string.className = "style-scope ytd-button-renderer style-default size-default";
   string.innerHTML = "Download";
+
   var place = document.getElementById("top-level-buttons");
   place.appendChild(btn);
 
@@ -69,37 +64,34 @@ window.onload = function(){
   var dropdown = document.createElement("div");
   dropdown.id = "download-dropdown";
   document.body.appendChild(dropdown);
+
   var droplist = document.createElement("ul");
   dropdown.appendChild(droplist);
 
-  var j=0; // for no links condition
-  var dropdown = document.getElementById("download-dropdown");
-  var ul = dropdown.children;
-  var li = ul[0].children;
-
-  for(i=0;i<videoDetailsCut.length;i++){
-    var item = document.createElement("a");
-    if (videoDetailsCut[i]['url'] == null){
-      j++;
-      continue;
-    }
-    item.setAttribute("href", videoDetailsCut[i]['url']);
-    item.setAttribute("target", "_blank");
-    item.setAttribute("data-type", videoDetailsCut[i]['mimeType']);
-    if(videoDetailsCut[i]['qualityLabel'])
-      item.innerText = videoDetailsCut[i]['qualityLabel'] + " " + videoDetailsCut[i]['mimeType'];
-    else
-      item.innerText = videoDetailsCut[i]['mimeType'];
-    item.addEventListener("click", getNameUrl);
-    droplist.appendChild(item);
+  for (var i = 0; i < videoDetailsCut.length; i++) {
+      var item = document.createElement("li"); // Change "a" to "li"
+      if (videoDetailsCut[i]['url'] == null) {
+          continue;
+      }
+      var link = document.createElement("a");
+      link.setAttribute("href", videoDetailsCut[i]['url']);
+      link.setAttribute("target", "_blank");
+      link.setAttribute("data-type", videoDetailsCut[i]['mimeType']);
+      if (videoDetailsCut[i]['qualityLabel']) {
+          link.innerText = videoDetailsCut[i]['qualityLabel'] + " " + videoDetailsCut[i]['mimeType'];
+      } else {
+          link.innerText = videoDetailsCut[i]['mimeType'];
+      }
+      link.addEventListener("click", getNameUrl);
+      item.appendChild(link);
+      droplist.appendChild(item);
   }
 
-  if (i==j)
-    btn.onclick=function(){
-      alert("Couldn't generate links for this video!!");
-    };
-  else
-    document.addEventListener("click", clicked);
-    /*btn.addEventListener("click", clickDownload);*/
-
-}
+  if (i === 0) {
+      btn.onclick = function () {
+          alert("Couldn't generate links for this video!!");
+      };
+  } else {
+      document.addEventListener("click", clicked);
+  }
+};
